f3ad4bc1e68da3a0044bf12c1c9bf28d3f2a9af5
mkcesar30 <michele.cesareo@dasir.it>
2025-02-18 18:54:18 +0100

    Update models.py

--- proj_library/library_app/models.py
+++ proj_library/library_app/models.py
@@ -4,7 +4,7 @@ class Editor(models.Model):
     editor = models.CharField(max_length=100)
     insert_date = models.DateField(auto_now_add=True)
     update_date = models.DateField(auto_now=True)
-    is_active = models.BooleanField(default=True) # Campo per disattivare senza cancellare
+    activate = models.BooleanField(default=True) # Campo per disattivare senza cancellare
 
     def __str__(self):
         return self.editor
