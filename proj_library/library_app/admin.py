123957e97a9443e6f3c608ac8a8023aa49ce2202
mkcesar30 <michele.cesareo@dasir.it>
2025-02-18 18:58:22 +0100

    Update admin.py

--- proj_library/library_app/admin.py
+++ proj_library/library_app/admin.py
@@ -6,5 +6,10 @@ class EditorAdmin(admin.ModelAdmin):
     list_display = ('editor', 'insert_date', 'update_date', 'activate')
     list_filter = ('activate',)
     search_fields = ('editor',)
+    
+    def activate(self, request, queryset): 
+        queryset.update(activate=True)
+    def deactivate(self, request, queryset): 
+        queryset.update(activate=False)
 
 admin.site.register(Editor, EditorAdmin)
