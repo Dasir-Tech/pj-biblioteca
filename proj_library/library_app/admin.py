95315807df8943a8684fe7417cea58f94691564f
mkcesar30 <michele.cesareo@dasir.it>
2025-02-18 12:48:51 +0100

    fist commit

--- proj_library/library_app/admin.py
+++ proj_library/library_app/admin.py
@@ -1,36 +1,10 @@
 from django.contrib import admin
-#from .models import Nome_model, elenco dei model
+from .models import Editor
 
-# RICORDA -> MIGRARE I MODEL !!!!
 
-# Register your models here.
+class EditorAdmin(admin.ModelAdmin):
+    list_display = ('editor', 'insert_date', 'update_date', 'is_active')
+    list_filter = ('is_active',)
+    search_fields = ('editor',)
 
-'''
-# PERSONALIZZARE il pannello Admin
-
-admin.site.register(Nome_model)
-
-class Nome_modelAdmin(admin.ModelAdmin):
-    list_display = ("title","author","genre",)
-    
-admin.site.register(Nome_model, Nome_modelAdmin)    
-'''
-
-admin.site.site_header = "Gestion Library"
-admin.site.site_title = "Admin - Library"
-admin.site.index_title = "Admin Control Pannel"
-
-'''
-#nome_file.CSS per personalizzare la pag dell' Admin
-
-class CustomAdmin(admin.AdminSite):
-    def get_urls(self):
-        return super().get_urls()
-    
-    class Media:
-        css = {
-            "all" : ("admin/css/custom.css",)
-        }
-        
-admin.site = CustomAdmin()   
-'''
+admin.site.register(Editor, EditorAdmin)
