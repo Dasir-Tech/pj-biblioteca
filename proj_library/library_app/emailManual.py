c7972e5e30b2f2a4683d3d20906a2e60a3a433a1
LauraCippy <lauracomparelli912@gmail.com>
2025-02-21 17:58:36 +0100

    Fare la email

--- proj_library/library_app/emailManual.py
+++ proj_library/library_app/emailManual.py
@@ -1,15 +1,14 @@
-"""
 from datetime import timedelta
-from django.core.mail import send_mail
+#from django.core.mail import send_mail
 from django.utils.timezone import now
 from .models import Loan
 
-loan = Loan.objects.get(pk=id)
-#user = User.objects.get(id = loan.user_ID)
+user = Loan.objects.select_related('user_ID').all
 
-date = now().date() + timedelta(days=2)
-loans = Loan.objects.filter(due_date = date, active = True, status = Loan.Status.ON_LOAN)
+for u in user:
+    print(u)
 
+'''
 def expiration_email(user,loan):
     user_email = user.email
     user_name = user.name
@@ -27,5 +26,4 @@ def expiration_email(user,loan):
         )
     except Exception as e:
         print(f"Error sending expiration notice to {user_email}: {e}")
-
-"""
+'''
